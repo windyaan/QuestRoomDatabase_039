e61eddd62d4a0812cb9605d7585cb719171df616
windyaan <windyanurmaretha@gmail.com>
2024-12-17 18:27:53 +0700

    Memanggil fungsi getAllMahasiswa

--- app/src/main/java/com/example/project/Repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/project/Repository/RepositoryMhs.kt
@@ -1,7 +1,11 @@
 package com.example.project.Repository
 
 import com.example.project.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
+
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+        fun getAllMhs(): Flow<List<Mahasiswa>>
 }
