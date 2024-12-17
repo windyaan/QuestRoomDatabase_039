4da618d2cac59a2b21b02cff7ee593e8e02728ea
windyaan <windyanurmaretha@gmail.com>
2024-12-17 18:44:22 +0700

    Mendefinisikan tujuan navigasi

--- app/src/main/java/com/example/project/ui/theme/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/project/ui/theme/navigation/AlamatNavigasi.kt
@@ -3,3 +3,19 @@ package com.example.project.ui.theme.navigation
 interface AlamatNavigasi {
     val route: String
 }
+
+//tujuan navigasi (destination) untuk halaman "Home"
+object DestinasiHome : AlamatNavigasi {
+    override val route = "home"
+}
+
+object DestinasiDetail :AlamatNavigasi {
+    override val route = "detail"
+    const val NIM = "nim"
+    val routesWithArg = "$route/{$NIM}"
+}
+object DestinasiUpdate : AlamatNavigasi {
+    override val route = "update"
+    const val NIM = "nim"
+    val routesWithArg = "$route/{$NIM}"
+}
