beab089df6f431dfd1a705e0d73f3913d921b159
windyaan <windyanurmaretha@gmail.com>
2024-12-17 18:23:49 +0700

    Menghapus data mhs tertentu dari db

--- app/src/main/java/com/example/project/data/DAO/MahasiswaDao.kt
+++ app/src/main/java/com/example/project/data/DAO/MahasiswaDao.kt
@@ -1,6 +1,7 @@
 package com.example.project.data.DAO
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.project.data.entity.Mahasiswa
@@ -17,5 +18,6 @@ interface MahasiswaDao {
     @Query("SELECT * FROM mahasiswa WHERE nim = :nim")
     fun getMahasiswa(nim: String) : Flow<Mahasiswa>
 
-
+    @Delete
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
 }
