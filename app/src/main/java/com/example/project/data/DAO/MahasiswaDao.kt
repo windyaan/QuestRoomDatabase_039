a097c0d6d0a0d0e9f7d7dba3fd3ced65ad2a76c5
windyaan <windyanurmaretha@gmail.com>
2024-12-17 18:22:56 +0700

    Mengambil data mhs berdasarkan NIM

--- app/src/main/java/com/example/project/data/DAO/MahasiswaDao.kt
+++ app/src/main/java/com/example/project/data/DAO/MahasiswaDao.kt
@@ -13,4 +13,9 @@ interface MahasiswaDao {
 
     @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
     fun getAllMahasiswa(): Flow<List<Mahasiswa>>
+
+    @Query("SELECT * FROM mahasiswa WHERE nim = :nim")
+    fun getMahasiswa(nim: String) : Flow<Mahasiswa>
+
+
 }
