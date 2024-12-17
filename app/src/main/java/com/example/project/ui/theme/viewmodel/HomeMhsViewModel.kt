de0453b21022d4d60c359422eb5dee9c234460e0
windyaan <windyanurmaretha@gmail.com>
2024-12-17 18:53:40 +0700

    Mengatur alur pengambilan data

--- app/src/main/java/com/example/project/ui/theme/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/project/ui/theme/viewmodel/HomeMhsViewModel.kt
@@ -2,7 +2,22 @@ package com.example.project.ui.theme.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.example.project.Repository.RepositoryMhs
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
 
 class HomeMhsViewModel (
     private val repositoryMhs: RepositoryMhs
-) : ViewModel(){}
+) : ViewModel(){
+
+    val homeUiState: StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
+        .filterNotNull()
+        .map {
+            HomeUiState (
+                listMhs = it.toList(),
+                isLoading = false,
+            )
+        }
+
+
+}
