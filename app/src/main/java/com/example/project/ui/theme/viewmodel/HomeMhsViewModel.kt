3c69a0b7b98a60c58103b49026a5a62e03d495bf
windyaan <windyanurmaretha@gmail.com>
2024-12-17 18:51:01 +0700

    Membuat class HomeMhsViewModel

---
+++ app/src/main/java/com/example/project/ui/theme/viewmodel/HomeMhsViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.project.ui.theme.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.project.Repository.RepositoryMhs
+
+class HomeMhsViewModel (
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel(){}
