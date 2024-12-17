a0a097860b579199d19663ac2f3b03200cdb6240
windyaan <windyanurmaretha@gmail.com>
2024-12-17 18:22:13 +0700

    Mengambil seluruh data mhs dlm db dan mengurutkannya

--- app/src/main/java/com/example/project/data/DAO/MahasiswaDao.kt
+++ app/src/main/java/com/example/project/data/DAO/MahasiswaDao.kt
@@ -2,10 +2,15 @@ package com.example.project.data.DAO
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.project.data.entity.Mahasiswa
+import java.util.concurrent.Flow
 
 @Dao
 interface MahasiswaDao {
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa(): Flow<List<Mahasiswa>>
 }
