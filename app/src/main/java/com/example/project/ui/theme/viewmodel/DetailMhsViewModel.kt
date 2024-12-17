bc920dc62ce5e0c384e039f3ada8fe3ff07e35cc
windyaan <windyanurmaretha@gmail.com>
2024-12-17 19:03:24 +0700

    Membuat file DetailMhsViewModel

---
+++ app/src/main/java/com/example/project/ui/theme/viewmodel/DetailMhsViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.project.ui.theme.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.project.Repository.RepositoryMhs
+
+class DetailMhsViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs,
+) : ViewModel() {
+
+}
